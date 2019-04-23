e6dc6744e01ce4bb5bc69635dfda004f07717eee
sammx343 <naitsabes29@gmail.com>
2019-04-22 19:18:05 -0500

    Added death animation and couple of fixes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce = 15000f;
     public float jumpTakeOffSpeed = 15f;
 
-    public enum PlayerAction { Idle, Run, Crunch, PostCrunch, Jump }
+    public enum PlayerAction { Idle, Run, Crunch, PostCrunch, Jump, Death }
     public PlayerAction playerAction = PlayerAction.Idle;
 
     private float velocityX = 0;
@@ -22,7 +22,10 @@ public class PlayerController : MonoBehaviour {
     float groundRadius = 0.2f;
     public LayerMask whatIsGround;
     public float ForceX = 0;
+    public float ForceY = 0;
 
+    public float forcePushY = 5f;
+    public float forcePushX = 20f;
     // Use this for initialization
     void Start ()
     {
@@ -48,20 +51,47 @@ public class PlayerController : MonoBehaviour {
         Vector2 move = Vector2.zero;
 
         velocityY = rgdb.velocity.y;
-        move.x = Input.GetAxis("Horizontal");
         
-        ForceX = ForceX < 1 ? 0 : (ForceX / ForceX) * Mathf.Abs(ForceX - 2);
+        ForceX = Mathf.Abs(ForceX) < 1 ? 0 : (ForceX / ForceX) * Mathf.Abs(ForceX - 1);
+        ForceY = Mathf.Abs(ForceY) < 1 ? 0 : (ForceY / ForceY) * Mathf.Abs(ForceY - 1);
+        
+        if(playerAction == PlayerAction.Death)
+        {
+            if (!grounded)
+            {
+                if(clipName != "AirDeath" && clipName != "AirDeathFalling" && clipName != "Death" && clipName != "StayDeath")
+                    animator.Play("AirDeath");
+            }
+            else if (grounded)
+            {
+                if (clipName == "AirDeath" || clipName == "AirDeathFalling")
+                {
+                    animator.Play("StayDeath");
+                }
+                else if(clipName != "Death" && clipName != "StayDeath")
+                {
+                    animator.Play("Death");
+                }
+            }
+        }
+        else
+        {
+            playerActions(clipName);
+            move.x = Input.GetAxis("Horizontal");
+        }
 
-        rgdb.velocity = new Vector2(move.x * maxSpeed + ForceX, rgdb.velocity.y);
-
+        rgdb.velocity = new Vector2(move.x * maxSpeed + ForceX, rgdb.velocity.y + ForceY);
         velocityX = Mathf.Abs(rgdb.velocity.x) / maxSpeed;
+    }
 
+    private void playerActions(string clipName)
+    {
         if (Input.GetKey("right"))
         {
             run("right");
             runAnimation();
         }
-        else if(Input.GetKey("left"))
+        else if (Input.GetKey("left"))
         {
             run("left");
             runAnimation();
@@ -118,7 +148,7 @@ public class PlayerController : MonoBehaviour {
 
     private void runAnimation()
     {
-        if (velocityX > 0.1f && grounded)
+        if (velocityX > 0.1f && grounded && velocityY < 0.1f)
         {
             animator.Play("Run");
             playerAction = PlayerAction.Run;
@@ -127,7 +157,7 @@ public class PlayerController : MonoBehaviour {
 
     private void idleAnimation(string clipName)
     {
-        if (grounded && velocityX < 0.1f && clipName != "PostCrunch")
+        if (grounded && velocityX < 0.1f && velocityY < 0.1f && clipName != "PostCrunch")
         {
             animator.Play("Idle");
             playerAction = PlayerAction.Idle;
@@ -162,7 +192,7 @@ public class PlayerController : MonoBehaviour {
 
     private void postCrunchAnimation()
     {
-        if (playerAction == PlayerAction.Crunch && grounded && velocityX < 0.1f)
+        if (playerAction == PlayerAction.Crunch && grounded)
         {
             playerAction = PlayerAction.PostCrunch;
             animator.Play("PostCrunch");
@@ -171,25 +201,26 @@ public class PlayerController : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Trap")
+        if (playerAction != PlayerAction.Death)
         {
-            StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out, "SampleScene"));
-        }
+            if (collision.gameObject.tag == "Trap")
+            {
+                var opossingBodyVelocity = collision.GetComponent<Rigidbody2D>().velocity;
 
-        if (collision.gameObject.tag == "PushingTotem")
-        {
-            //transform.Translate(15, 0, 0, Space.World);
-            Debug.Log("Touching");
-            Vector2 impulse = new Vector2(5.0f, 5.0f);
-            ForceX = 60f;
-            //rgdb.velocity = new Vector2(30.0f, 0);
-            //pushObject();
-        }
+                Debug.Log(collision.GetComponent<Rigidbody2D>().velocity);
+                playerAction = PlayerAction.Death;
 
-        if (collision.gameObject.tag == "DestroyablePlatform")
-        {
-            //Debug.Log("DESTROY");
-            //Destroy(collision.gameObject);
+                ForceY = (opossingBodyVelocity.y > 0) ? opossingBodyVelocity.y : velocityY;
+                ForceX = (opossingBodyVelocity.x > 0) ? opossingBodyVelocity.x : velocityX;
+                
+                StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out, "SampleScene"));
+            }
+
+            if (collision.gameObject.tag == "PushingTotem")
+            {
+                //transform.Translate(15, 0, 0, Space.World);
+                ForceX = 50f;
+            }
         }
     }
 }
